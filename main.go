116c87e4e796f417b804e5a712bcec43cb81add8
Zach Snyder <sadin@fedoraproject.org>
2022-05-20 11:07:36 -0400

    added boilerplate code for guild events and message events

--- main.go
+++ main.go
@@ -14,10 +14,41 @@ var (
 	Token string
 	buffer = make([][]byte, 0)
 )
+// global variables
+var guild_list []string
 
 func main() {
 	fmt.Println("discorder")
 
 	// launch discord session
+	fmt.Print("creating new discord session...")
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
 
+	// Register guildCreate as a callback for the guildCreate events.
+	dg.AddHandler(guildCreate)
+	// register message create event handler
+	dg.AddHandler(messageCreate)
+}
+
+func guildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
+
+	if event.Guild.Unavailable {
+		fmt.Println("error with guild" + event.Guild.ID)
+		return
+	}
+	guild_list = append(guild_list,event.Guild.Name)
+	fmt.Println(event.Guild.Name)
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// ignore messages sent by bot
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	fmt.Println("Message detected")
 }
